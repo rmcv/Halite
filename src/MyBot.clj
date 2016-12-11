71a261148f908afa3a3b5314197756ee88ba6401
Rimon <rimonchi@rimons-imac-4.local>
2016-12-12 02:53:34 +0800

    logging

--- src/MyBot.clj
+++ src/MyBot.clj
@@ -142,18 +142,20 @@
                  teams))))
 
 (defn -main []
-  (timbre/merge-config!
-   {:appenders {:spit (appenders/spit-appender {:fname "app.log"})}})
+  (do
+    (timbre/merge-config!
+     {:appenders {:spit (appenders/spit-appender {:fname "app.log"})}})
 
-  (info "Starting...")
+    (info "Starting...")
 
-  (let [{:keys [my-id productions width height game-map]} (io/get-init!)]
+    (let [{:keys [my-id productions width height game-map]} (io/get-init!)]
 
-    ;; Do any initialization you want with the starting game-map before submitting the bot-name
+      ;; Do any initialization you want with the starting game-map before submitting the bot-name
 
-    (println bot-name)
+      (println bot-name)
 
-    (doseq [turn (range)]
-      (let [game-map (io/create-game-map width height productions (io/read-ints!))]
-        (io/send-moves! (move my-id game-map))))))
+      (doseq [turn (range)]
+        (let [game-map (io/create-game-map width height productions (io/read-ints!))]
+          (io/send-moves! (moves/improve-moves-2 my-id game-map)))))))
+
 
